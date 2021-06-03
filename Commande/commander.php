ef92ba2a4b678f4fa3137e77f6ce308df42e2eb4
davidcabu <David.cabu@zsdinaphi.be>
2021-06-03 13:04:45 +0200

    David
    qetg

---
+++ Commande/commander.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+  <link rel="stylesheet" href="" />
+
+  </head>
+
+  <body>
+
+
+
+    <H1>Introduisez votre commande :</H1>
+
+    <H3>Client :</H3>
+
+
+
+<p><select name="liste">
+
+     <option value="1">champ 1</option>
+
+     <option value="2">champ 2</option>
+
+     <option value="3">champ 3</option>
+
+     <option value="4">champ 4</option>
+
+</select></p>
+
+
+
+  
+
+
+
+  </body>
+
+</html>
---
+++ Commande/registration/logout.php
@@ -0,0 +1,21 @@
+<?php
+
+  // Initialiser la session
+
+  session_start();
+
+  
+
+  // Détruire la session.
+
+  if(session_destroy())
+
+  {
+
+    // Redirection vers la page de connexion
+
+    header("Location: https://ksfsoftware.000webhostapp.com/Commande/registration/login.php");
+
+  }
+
+?>
