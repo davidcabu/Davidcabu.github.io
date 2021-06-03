019f79e18bc713a0fb3795836781ea61062afa86
davidcabu <David.cabu@zsdinaphi.be>
2021-06-03 13:24:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
   if(!isset($_SESSION["username"])){
 
-    header("Location: https://ksfsoftware.000webhostapp.com/Commande/registration/login.php");
+    header("Location: Commande/registration/login.php");
 
     exit(); 
 
@@ -22,7 +22,7 @@
 
   <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <link rel="stylesheet" href="https://ksfsoftware.000webhostapp.com/Commande/registration/style.css" />
+  <link rel="stylesheet" href="Commande/registration/style.css" />
 
   </head>
 
@@ -34,34 +34,34 @@
 
       <h3>Sélectionnez une tâche :</h3>
 
-        <form name="commander" action="https://ksfsoftware.000webhostapp.com/Commande/commander.php" method="post">
+        <form name="commander" action="Commande/commander.php" method="post">
 
           <input type="submit" value="Nouvelle commande">
 
         </form><br>
 
-          <form name="client" action="https://ksfsoftware.000webhostapp.com/Commande/gestionClient.php" method="post">
+          <form name="client" action="Commande/gestionClient.php" method="post">
 
             <input type="submit" value="Gestion client">
 
           </form><br>
 
-            <form name="commande" action="https://ksfsoftware.000webhostapp.com/Commande/gestionCommande.php" method="post">
+            <form name="commande" action="Commande/gestionCommande.php" method="post">
 
               <input type="submit" value="Gérer les commandes">
 
             </form><br> 
 
-              <form name="article" action="https://ksfsoftware.000webhostapp.com/Commande/gestionArticle.php" method="post">
+              <form name="article" action="Commande/gestionArticle.php" method="post">
 
                 <input type="submit" value="Gérer les articles">
 
               </form><br> 
 
-	      <button onclick="window.location.href = 'https://ksfsoftware.000webhostapp.com/Commande/gestionArticle.php';">Gérer les articles</button><br><br>
+	      <button onclick="window.location.href = 'Commande/gestionArticle.php';">Gérer les articles</button><br><br>
 			        
 
-      <a href="https://ksfsoftware.000webhostapp.com/Commande/registration/logout.php">Déconnexion</a>
+      <a href="Commande/registration/logout.php">Déconnexion</a>
 
     </div>
 
